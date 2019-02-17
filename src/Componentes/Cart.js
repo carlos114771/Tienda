a151309a2ddee0ba0e9d723ed25d9c9b92332576
carlos114771 <carlosalfredoriveramilian@yahoo.es>
2019-02-17 14:33:50 -0600

    agregarando mas clases

---
+++ src/Componentes/Cart.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import Navbar from "./NavBar.js";
+
+export default class Cart extends React.Component {
+    render() {
+        return (
+            <div>
+                <Navbar />
+                
+            </div>
+        );
+    }
+}
---
+++ src/Componentes/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { Container, Row, Col } from "reactstrap";
+import Navbar from "./NavBar.js";
+
+class Home extends Component {
+  render() {
+    
+    return (
+      <div>
+        <Navbar />
+        
+      </div>
+    );
+  }
+}
+
+export default Home;

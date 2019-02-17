b4a6d7bfbce3055846b6e82ccfa4fdbcaa162dae
carlos114771 <carlosalfredoriveramilian@yahoo.es>
2019-02-17 14:24:22 -0600

    creacion del navigation bar

---
+++ src/Componentes/NavBar.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import logo from './logouber.PNG';
+import SignIn from './SignIn.js';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+  import {Link} from 'react-router-dom';
+
+export default class NavigationBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/"><img src={logo} height="130" alt="logo"/> </NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <Link to={"/SignIn.js/"}>SignIn</Link>
+              </NavItem>
+              <NavItem>
+              <Link to={"/Cart.js/"}>Cart</Link>
+              </NavItem>
+             
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
